7efbffb21a129db2e0b95c9f921a7c235aaa3cde
Michael <mrbaez427@gmail.com>
2025-10-05 23:13:37 -0700

    Created new service file and functions for later

---
+++ backend/services/spotifyService.ts
@@ -0,0 +1,9 @@
+export async function initialSpotifyUserDataSync() {}
+
+export async function reSyncSpotifyUserData() {}
+
+export async function fetchUserLikedTracks() {}
+
+export async function fetchUserRecentlyPlayed() {}
+
+export async function fetchUserPlaylists() {}
